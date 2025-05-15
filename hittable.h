0906ca6d7e43fd20d3a3d06a2e40410622f19707
Mitchell <mitchbowell12@gmail.com>
2025-05-15 14:23:44 +1000

    6.8

--- hittable.h
+++ hittable.h
@@ -3,6 +3,7 @@
 
 //#include "ray.h"
 #include "rtweekend.h"
+#include "interval.h"
 
 class hit_record {
   public:
@@ -24,7 +25,9 @@ class hittable {
   public:
     virtual ~hittable() = default;
 
-    virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
-};
+    //virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+    
+    virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
+  };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -6,13 +6,14 @@
 #include "hittable.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "interval.h"
 
 #include <fstream>
 #include <iostream>
 
 color ray_color(const ray& r, const hittable& world) {
     hit_record rec;
-    if (world.hit(r, 0, infinity, rec)) {
+    if (world.hit(r, interval(0, infinity), rec)) {
         return 0.5 * (rec.normal + color(1,1,1));
     }
 
