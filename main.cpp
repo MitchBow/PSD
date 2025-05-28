7ff9e83bb40f99c25928215a852b628c5f4af389
Mitchell <mitchbowell12@gmail.com>
2025-05-28 14:22:06 +1000

    small clean

--- main.cpp
+++ main.cpp
@@ -41,16 +41,11 @@ int main() {
     world.add(make_shared<sphere>(point3(1.0, 0.1, -1.0), 0.5, checker_sphere_mat));
 
 
-    //auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
-    //auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
-
-    //world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5)); //matt colour
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8)); //reflective metal
+    
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-   // world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
-
 
     camera cam;
 
@@ -64,7 +59,6 @@ int main() {
         cam.samples_per_pixel = 30;
     }
 
-    
     cam.max_depth = 50;
 
     cam.render(world);
